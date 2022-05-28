7a90c6fb8a5499399f2e6244ce5476d4bf928b5a
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-28 16:05:40 +0300

    3

--- LAB_4/Part2/Service/StudentDTO.java
+++ LAB_4/Part2/Service/StudentDTO.java
@@ -1,7 +1,40 @@
 package LAB_4.Part2.Service;
 
 public class StudentDTO {
+    Integer id;
     String name;
     String group;
+    Grades grades;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name ="Name: " + name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = "Group" +  group;
+    }
+
+    public Grades getGrades() {
+        return grades;
+    }
+
+    public void setGrades(Grades grades) {
+        this.grades = grades;
+    }
 }
